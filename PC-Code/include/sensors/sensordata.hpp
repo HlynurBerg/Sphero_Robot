75a40cafd787f14ac72649672a43a8450de8bad6
Hlynur <hlynur.berg@gmail.com>
2023-11-19 17:15:25 +0100

    Seperated into .hpp and .cpp files. removed using namespace.

--- PC-Code/include/sensors/sensordata.hpp
+++ PC-Code/include/sensors/sensordata.hpp
@@ -1,76 +1,40 @@
-//
-// Created by jorge on 11/9/2023.
-//
-
 #ifndef SPHERO_ROBOT_SENSORDATA_HPP
 #define SPHERO_ROBOT_SENSORDATA_HPP
-
-//TODO: All the below works in the locally test code, but not in the actual code. Fix it <3
+#include <SDL.h> //for User Input. Change when switching to html5 api
+#include <boost/asio.hpp> //for communicatoin
+#include <opencv2/opencv.hpp> //for image processing
+#include <iostream>
+#include <string>
+#include <boost/array.hpp>
+#include <thread>
+#include <vector>
+#include <cmath>
 #include <nlohmann/json.hpp>
 
-using boost::asio::ip::tcp;
-using json = nlohmann::json;
+float colorTracker(cv::Mat frame);
 
 class DataReceiver {
 public:
-    DataReceiver(const std::string& host, int port)
-        : io_service_(), socket_(io_service_), endpoint_(tcp::endpoint(boost::asio::ip::address::from_string(host), port)) {
-        connect();
-    }
+    DataReceiver(const std::string& host, int port);
 
-    void connect() {
-        socket_.connect(endpoint_);
-    }
-
-    void updateData() {
-        boost::system::error_code error;
-        while(socket_.available()) {
-            size_t len = socket_.read_some(boost::asio::buffer(recv_buf_), error);
-            if (error && error != boost::asio::error::eof) {
-                std::cerr << "Receive failed: " << error.message() << std::endl;
-                return;
-            }
-            data_buffer_ += std::string(recv_buf_.data(), len);
-        }
-
-        // Process complete JSON messages
-        while(true) {
-            size_t pos = data_buffer_.find('\n');
-            if (pos != std::string::npos) {
-                std::string json_message = data_buffer_.substr(0, pos);
-                data_buffer_.erase(0, pos + 1);
-                parseData(json_message);
-            } else {
-                break;
-            }
-        }
-    }
-
-    double getBatteryPercentage() const { return battery_percentage_; }
-    double getDistanceMm() const { return distance_mm_; }
-    double getSpeedY() const { return speed_y_; }
+    void connect();
+    void updateData();
+    double getBatteryPercentage() const;
+    double getDistanceMm() const;
+    double getSpeedY() const;
 
 private:
     boost::asio::io_service io_service_;
-    tcp::socket socket_;
-    tcp::endpoint endpoint_;
+    boost::asio::ip::tcp::socket socket_;
+    boost::asio::ip::tcp::endpoint endpoint_;
     boost::array<char, 1024> recv_buf_;
     std::string data_buffer_;
     double battery_percentage_;
     double distance_mm_;
     double speed_y_;
 
-    void parseData(const std::string& data) {
-        try {
-            auto j = json::parse(data);
-            battery_percentage_ = j["Battery"]["percentage"];
-            distance_mm_ = j["Distance"];
-            speed_y_ = j["Speed"]["Velocity"]["Y"];
-        } catch (const std::exception& e) {
-            std::cerr << "JSON Parsing error: " << e.what() << std::endl;
-            std::cerr << "Received data: " << data << std::endl; // For debugging
-        }
-    }
+    void parseData(const std::string& data);
 };
+
 
 #endif//SPHERO_ROBOT_SENSORDATA_HPP
---
+++ PC-Code/src/sensors/sensorreader.cpp
@@ -0,0 +1,129 @@
+#include <sensors/colortracker.hpp>
+#include <sensors/sensordata.hpp>
+
+float colorTracker(cv::Mat image) {
+    try {
+        //TODO: Define color bounds as a variable, let used decide
+        cv::Scalar lower_bound(13, 175, 50);
+        cv::Scalar upper_bound(23, 255, 255);
+        // Define smallest detected object
+        int min_contour_area = 500;
+
+        cv::Mat hsv, mask, segmented;
+        std::vector<std::vector<cv::Point>> contours;
+
+        if (image.empty()) {
+            std::cerr << "Could not decode frame!" << std::endl;
+        }
+
+        // Color and contour detection
+        cvtColor(image, hsv, cv::COLOR_BGR2HSV);
+        inRange(hsv, lower_bound, upper_bound, mask);
+        bitwise_and(image, image, segmented, mask);
+        findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        double weightedSumX = 0.0, weightedSumY = 0.0, totalArea = 0.0;
+
+        for (const auto &contour: contours) {
+            double area = contourArea(contour);
+            if (area > min_contour_area) {
+                cv::Moments M = moments(contour);
+                if (M.m00 != 0) {
+                    cv::Point2f centroid(static_cast<float>(M.m10 / M.m00), static_cast<float>(M.m01 / M.m00));
+                    weightedSumX += centroid.x * area;
+                    weightedSumY += centroid.y * area;
+                    totalArea += area;
+
+                    // Draw circle and label, if showing image
+                    /*
+                    circle(image, centroid, 5, cv::Scalar(255, 0, 0), -1);
+                    std::string label = "Size: " + std::to_string(area) + " px";
+                    putText(image, label, centroid, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 1,
+                            cv::LINE_AA);
+                    */
+                }
+            }
+        }
+
+        if (totalArea > 0) {
+            cv::Point2f overallCentroid(static_cast<float>(weightedSumX / totalArea),
+                                        static_cast<float>(weightedSumY / totalArea));
+
+            // Draw the overall centroid
+            // circle(image, overallCentroid, 10, cv::Scalar(0, 0, 255), -1);
+
+            // Calculate the horizontal difference from the center of the screen
+            float screenCenterX = segmented.cols / 2.0f;
+            float difference = overallCentroid.x - screenCenterX;
+
+            std::cout << "Difference: " << difference << std::endl;
+
+            return difference;
+        }
+    }
+
+
+    catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 0;
+    }
+}
+
+
+// Definitions of member functions of DataReceiver
+DataReceiver::DataReceiver(const std::string& host, int port)
+    : io_service_(), socket_(io_service_), endpoint_(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(host), port)) {
+    connect();
+}
+
+void DataReceiver::connect() {
+    socket_.connect(endpoint_);
+}
+
+void DataReceiver::updateData() {
+    boost::system::error_code error;
+    while(socket_.available()) {
+        size_t len = socket_.read_some(boost::asio::buffer(recv_buf_), error);
+        if (error && error != boost::asio::error::eof) {
+            std::cerr << "Receive failed: " << error.message() << std::endl;
+            return;
+        }
+        data_buffer_ += std::string(recv_buf_.data(), len);
+    }
+
+    // Process complete JSON messages
+    while(true) {
+        size_t pos = data_buffer_.find('\n');
+        if (pos != std::string::npos) {
+            std::string json_message = data_buffer_.substr(0, pos);
+            data_buffer_.erase(0, pos + 1);
+            parseData(json_message);
+        } else {
+            break;
+        }
+    }
+}
+
+double DataReceiver::getBatteryPercentage() const {
+    return battery_percentage_;
+}
+
+double DataReceiver::getDistanceMm() const {
+    return distance_mm_;
+}
+
+double DataReceiver::getSpeedY() const {
+    return speed_y_;
+}
+
+void DataReceiver::parseData(const std::string& data) {
+    try {
+        auto j = nlohmann::json::parse(data);
+        battery_percentage_ = j["Battery"]["percentage"];
+        distance_mm_ = j["Distance"];
+        speed_y_ = j["Speed"]["Velocity"]["Y"];
+    } catch (const std::exception& e) {
+        std::cerr << "JSON Parsing error: " << e.what() << std::endl;
+        std::cerr << "Received data: " << data << std::endl; // For debugging
+    }
+}
