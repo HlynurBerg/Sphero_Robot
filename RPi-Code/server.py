cc313986654355c0bdd90284514d2e95472c8b9c
aspr3m <robert.asprem@gmail.com>
2023-12-14 18:37:12 +0100

    working server code

--- RPi-Code/server.py
+++ RPi-Code/server.py
@@ -7,6 +7,8 @@ import imutils
 import base64
 import time
 import qwiic
+import json
+import signal
 
 sys.path.append('/home/pi/sphero-sdk-raspberrypi-python')
 from sphero_sdk import SpheroRvrAsync, SerialAsyncDal, RvrStreamingServices
@@ -18,9 +20,9 @@ VIDEO_PORT = 6001
 VIDEO_BUFF_SIZE = 65536
 COMMAND_SERVER_PORT = 6000
 DATA_BROADCAST_PORT = 6003
-FRAME_WIDTH = 640
-FRAME_HEIGHT = 480
-JPEG_QUALITY = 30
+FRAME_WIDTH = 320
+FRAME_HEIGHT = 240
+JPEG_QUALITY = 25
 BATTERY_UPDATE_INTERVAL = 30  # seconds
 
 # Global variables
@@ -31,6 +33,11 @@ speed_data = {}  # Global variable for the velocity data from the RVR
 encoder_data = {}  # Global variable for encoder data from the RVR
 battery_percentage = 0  # Global variable for battery percentage from the RVR
 distance_mm = 0  # Global variable for distance in millimeters from the sparkfun TOF
+rvr_controller_thread_handle = None
+video_stream_thread_handle = None
+distance_thread_handle = None
+data_thread_handle = None
+threads_initialized = False # Global flag for thread initialization
 
 # AsyncIO loop and SpheroRvrAsync instance
 loop = asyncio.get_event_loop()
@@ -38,9 +45,9 @@ rvr = SpheroRvrAsync(dal=SerialAsyncDal(loop))
 
 
 def stop():
-    """Stops the main loop and all threads."""
     global running
     running = False
+    print("Stop signal received, setting running to False.")
 
 
 def process_command(command):
@@ -62,6 +69,8 @@ def rvr_controller_thread():
         server_socket.listen(1)
         print("Command server is running and waiting for a connection...")
 
+        server_socket.settimeout(1)  # Set timeout
+
         # Command server loop
         while running:
             client_socket, address = None, None
@@ -78,6 +87,8 @@ def rvr_controller_thread():
                     for command in commands:
                         if command:
                             process_command(command)
+            except socket.timeout:
+                continue  # Check the running flag every 1 second
             except Exception as e:
                 print(f"An error occurred with the client {address}: {e}")
             finally:
@@ -88,84 +99,88 @@ def rvr_controller_thread():
 
 def video_stream_thread():
     """Thread for handling video streaming via UDP."""
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, VIDEO_BUFF_SIZE)
+    vid = cv2.VideoCapture(0, cv2.CAP_V4L2)
+    if not vid.isOpened():
+        print("Cannot open camera. Exiting video thread.")
+        return  # Exit the thread if the camera cannot be opened
 
-    try:
-        server_socket.bind((HOST_IP, VIDEO_PORT))
-    except Exception as e:
-        print(f"Cannot bind to address {HOST_IP}:{VIDEO_PORT}, error: {e}")
-        server_socket.close()
-        return
+    vid.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
+    vid.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
 
-    print(f'Video server listening at: {HOST_IP}:{VIDEO_PORT}')
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, VIDEO_BUFF_SIZE)
 
-    vid = None
-    try:
-        print('Waiting for client to connect for video stream...')
-        msg, client_addr = server_socket.recvfrom(VIDEO_BUFF_SIZE)
-        print(f'Video connection from {client_addr}')
-
-        vid = cv2.VideoCapture(0, cv2.CAP_V4L2)
-        if not vid.isOpened():
-            print("Cannot open camera.")
+        try:
+            server_socket.bind((HOST_IP, VIDEO_PORT))
+        except Exception as e:
+            print(f"Cannot bind to address {HOST_IP}:{VIDEO_PORT}, error: {e}")
+            vid.release()
             return
 
-        vid.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
-        vid.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
+        print(f'Video server listening at: {HOST_IP}:{VIDEO_PORT}')
 
         while running:
-            ret, frame = vid.read()
-            if not ret:
-                print("Failed to grab frame.")
-                break
-
-            frame = imutils.resize(frame, width=FRAME_WIDTH)
-            _, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY])
-            message = base64.b64encode(buffer)
             try:
-                server_socket.sendto(message, client_addr)
+                print('Waiting for client to connect for video stream...')
+                _, client_addr = server_socket.recvfrom(VIDEO_BUFF_SIZE)
+                print(f'Video connection from {client_addr}')
+
+                while running:
+                    ret, frame = vid.read()
+                    if not ret:
+                        print("Failed to grab frame.")
+                        break
+
+                    frame = imutils.resize(frame, width=FRAME_WIDTH)
+                    _, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY])
+                    message = base64.b64encode(buffer)
+                    server_socket.sendto(message, client_addr)
+
             except Exception as e:
-                print(f"An error occurred while sending video: {e}")
-                break
-    except Exception as e:
-        print(f"An error occurred in video_thread: {e}")
-    finally:
-        if vid:
-            vid.release()
-        server_socket.close()
-        print("Video server closed.")
+                print(f"An error occurred in video_thread: {e}")
+
+    vid.release()
+    print("Video server closed.")
+
 
 
 def data_broadcast_thread():
     """Thread for broadcasting sensor data via TCP."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind(('0.0.0.0', DATA_BROADCAST_PORT))
         server_socket.listen(1)
         print("Data broadcast server running...")
 
-        client_socket, _ = server_socket.accept()
-        with client_socket:
-            print("Data broadcast client connected")
+        server_socket.settimeout(1)  # Set timeout for accept operation
 
-            while running:
-                if speed_data or encoder_data or battery_percentage or distance_mm:
-                    try:
-                        # Combine data from the different sensors
-                        combined_data = {
-                            'timestamp': time.time(),
-                            'Speed': speed_data,
-                            'Encoder': encoder_data,
-                            'Battery': battery_percentage,
-                            'Distance': distance_mm
-                        }
-                        data_str = str(combined_data) + '\n'
-                        client_socket.sendall(data_str.encode('utf-8'))
-                        # print(f"Sent data: {data_str}")  # Debugging print
-                    except Exception as e:
-                        print(f"Error sending data: {e}")
-                        break
+        while running:
+            try:
+                client_socket, _ = server_socket.accept()
+                with client_socket:
+                    print("Data broadcast client connected")
+
+                    while running:
+                        if speed_data or encoder_data or battery_percentage or distance_mm:
+                            try:
+                                # Combine data from the different sensors
+                                combined_data = {
+                                    'timestamp': time.time(),
+                                    'Speed': speed_data,
+                                    'Encoder': encoder_data,
+                                    'Battery': battery_percentage,
+                                    'Distance': distance_mm
+                                }
+                                data_str = json.dumps(combined_data) + '\n'
+                                client_socket.sendall(data_str.encode('utf-8'))
+                            except Exception as e:
+                                print(f"Error sending data: {e}")
+                                break
+            except socket.timeout:
+                continue  # Check the running flag every 1 second
+            except Exception as e:
+                print(f"Data broadcast thread error: {e}")
 
 
 async def speed_handler(data):
@@ -203,10 +218,16 @@ def measure_distance():
             print(e)
         time.sleep(0.1)  # Adjust the frequency of measurements as needed
 
+def signal_handler(sig, frame):
+    print('\nSignal received, stopping...')
+    stop()
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
 
 async def main():
     """Main async function to set up and run the RVR and its servers."""
-    global left_velocity, right_velocity, encoder_data
+    global rvr_controller_thread_handle, video_stream_thread_handle, distance_thread_handle, data_thread_handle, threads_initialized
 
     await rvr.wake()
     await asyncio.sleep(2)
@@ -223,14 +244,16 @@ async def main():
     battery_percentage_task = asyncio.create_task(update_battery_percentage())
 
     # Starting threads for server, video, distance and data broadcast
-    rvr_controller_thread_handle = threading.Thread(target=rvr_controller_thread, daemon=True)
+    rvr_controller_thread_handle = threading.Thread(target=rvr_controller_thread, daemon=True, name="rvr_controller_thread"
     rvr_controller_thread_handle.start()
-    video_stream_thread_handle = threading.Thread(target=video_stream_thread, daemon=True)
+    video_stream_thread_handle = threading.Thread(target=video_stream_thread, daemon=True, name="video_stream_thread")
     video_stream_thread_handle.start()
-    distance_thread_handle = threading.Thread(target=measure_distance, daemon=True)
+    distance_thread_handle = threading.Thread(target=measure_distance, daemon=True, name="distance_thread")
     distance_thread_handle.start()
-    data_thread_handle = threading.Thread(target=data_broadcast_thread, daemon=True)
+    data_thread_handle = threading.Thread(target=data_broadcast_thread, daemon=True, name="data_thread"
     data_thread_handle.start()
+
+    threads_initialized = True
 
     # Main loop for driving the RVR
     while running:
@@ -242,8 +265,7 @@ async def main():
 
     # Cleanup
     battery_percentage_task.cancel()
-    rvr_controller_thread_handle.join()
-    video_stream_thread_handle.join()
+
     await rvr.close()
 
 
@@ -254,5 +276,21 @@ if __name__ == '__main__':
         print('\nProgram terminated with keyboard interrupt.')
         stop()
     finally:
-        loop.run_until_complete(loop.shutdown_asyncgens())
+        # Cancel all running tasks and close the loop
+        for task in asyncio.all_tasks(loop):
+            task.cancel()
+        loop.run_until_complete(asyncio.gather(*asyncio.all_tasks(loop), return_exceptions=True))
         loop.close()
+
+        # Now join the threads after the event loop is closed
+        if threads_initialized:
+            if rvr_controller_thread_handle is not None:
+                rvr_controller_thread_handle.join()
+            if video_stream_thread_handle is not None:
+                video_stream_thread_handle.join()
+            if distance_thread_handle is not None:
+                distance_thread_handle.join()
+            if data_thread_handle is not None:
+                data_thread_handle.join()
+
+        print("All threads have been successfully stopped.")
